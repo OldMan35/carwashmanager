194f178ba0c54260a06d8e44e75e8d6f7c0361fc
OldMan35 <kostya_kv@mail.ru>
2021-07-29 00:42:40 +0300

    add annotation @Data in User.java

--- src/main/java/ru/car/carwashmanager/model/User.java
+++ src/main/java/ru/car/carwashmanager/model/User.java
@@ -1,8 +1,10 @@
 package ru.car.carwashmanager.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
-
+@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -33,67 +35,4 @@ public class User {
     @Column(name = "enabled")
     private boolean enabled;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserSurname() {
-        return userSurname;
-    }
-
-    public void setUserSurname(String userSurname) {
-        this.userSurname = userSurname;
-    }
-
-    public String getTelephoneNumber() {
-        return telephoneNumber;
-    }
-
-    public void setTelephoneNumber(String telephoneNumber) {
-        this.telephoneNumber = telephoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
 }
